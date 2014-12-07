0c48b3d4f49f152c02d47eff8d1260c70da758ae
OssiB <ossi.bister@gmail.com>
2014-12-07 11:02:57 +0200

    mapper

---
+++ src/main/java/hadoop/meetup/first/StateReputationMapper.java
@@ -0,0 +1,22 @@
+package hadoop.meetup.first;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class StateReputationMapper extends
+		Mapper<LongWritable, Text, Text, IntWritable> {
+
+	@Override
+	protected void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+		List<String> states = Arrays.asList("AZ","FL","CA","IL","IN","MD","MA","GA","MI","MO","NC","NJ","NY","OH","PA","TN","TX","VA","WA","WI");
+
+	}
+}
