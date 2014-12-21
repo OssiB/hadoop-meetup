2e6fdd58c0202f15c33801712784c3690c3efbec
OssiB <ossi.bister@gmail.com>
2014-12-21 08:35:24 -0800

    modified

--- src/main/java/hadoop/meetup/first/StateReputationMapper.java
+++ src/main/java/hadoop/meetup/first/StateReputationMapper.java
@@ -12,16 +12,28 @@ import org.apache.hadoop.mapreduce.Mapper;
 
 public class StateReputationMapper extends
 		Mapper<LongWritable, Text, Text, IntWritable> {
-			
-        enum Reputation {HEADER,NOT_IN_STATES}
-        
+
+	enum Reputation {
+		HEADER, NOT_IN_STATES
+	}
+
 	@Override
 	protected void map(LongWritable key, Text value, Context context)
 			throws IOException, InterruptedException {
-		List<String> states = Arrays.asList("AZ","FL","CA","IL","IN",
-				                    "MD","MA","GA","MI","MO",
-				                    "NC","NJ","NY","OH","PA",
-				                    "TN","TX","VA","WA","WI");
+		List<String> states = Arrays.asList("AZ", "FL", "CA", "IL", "IN", "MD",
+				"MA", "GA", "MI", "MO", "NC", "NJ", "NY", "OH", "PA", "TN",
+				"TX", "VA", "WA", "WI");
+		String line = value.toString();
+		StackExchangeRecord record = new StackExchangeRecord(line);
+		String parsedState = record.getState();
+		if (states.contains(parsedState)) {
+			context.write(new Text(parsedState), new IntWritable(new Integer(
+					record.getReputation())));
 
+		} else if (record.isHeader()) {
+			context.getCounter(Reputation.HEADER).increment(1);
+		} else {
+			context.getCounter(Reputation.NOT_IN_STATES).increment(1);
+		}
 	}
 }
